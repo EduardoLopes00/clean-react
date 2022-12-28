aa614661adf65146bc9fbafd6bb302d242c390e8
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-12-28 15:29:33 -0300

    chore: add Jest conifg

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/jestSetup.ts"],
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
     "^.+\\.(ts|tsx)?$": "ts-jest",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/fileTransformer.js",
   },
   testEnvironment: "jsdom",
   moduleNameMapper: {
